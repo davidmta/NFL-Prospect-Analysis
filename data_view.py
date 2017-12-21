fb8dc934df6d3a24f138240ba321bcc7743696a9
David Ta <dta255@stanford.edu>
2017-12-20 22:28:48 -0800

    sr_scrape can parse url

--- data_view.py
+++ data_view.py
@@ -5,16 +5,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(style="ticks", color_codes=True)
 
+PRO_SPEED_CATEGORY_NUM = 13
+
 ### ----------------------------------------------------------------------------------------------------
 ### SPEED_COMP
 ### ----------------------------------------------------------------------------------------------------
 
-def sort_by_conference(conn)
-    SEC = ["Alabama","Florida","Georgia","Missouri","South Carolina","Tennessee","Vanderbilt","Alabama","Arkansas","Auburn","LSU","Mississippi","Mississippi State","Texas AM"]
-    BIGTEN = ["Indiana","Maryland","Michigan","Michigan State","Ohio State","Penn State","Rutgers","Illinois","Iowa","Minnesota","Nebraska","Northwestern","Purdue","Wisconsin"]
+def get_conf_stats(conference, category, conference_dict):
+    stats = []
+    for college in conference_dict[conference]:
+        for player_stats in conference_dict[conference][college]:
+            if player_stats[category] != "NULL":
+                stats.append(float(player_stats[category]))
+    return stats
+
+def plot_speedgraph(SECspeed_df,BIG10speed_df,ACCspeed_df,PAC12speed_df,BIG12speed_df):
+    speed_df = [SECspeed_df,BIG10speed_df,ACCspeed_df,PAC12speed_df,BIG12speed_df]
+    speed_df = pd.concat(speed_df, axis=1)
+    speed_df.plot.box()
+    plt.show()
 
 def speed_comp(conn):
-    sort_by_conference(conn)
+    conference_dict = sort_by_conference(conn)
+    SECspeed_df = pd.DataFrame(get_conf_stats("SEC",PRO_SPEED_CATEGORY_NUM,conference_dict),columns=["SEC"])
+    BIG10speed_df = pd.DataFrame(get_conf_stats("BIG10",PRO_SPEED_CATEGORY_NUM,conference_dict),columns=["BIG10"])
+    ACCspeed_df = pd.DataFrame(get_conf_stats("ACC",PRO_SPEED_CATEGORY_NUM,conference_dict),columns=["ACC"])
+    PAC12speed_df = pd.DataFrame(get_conf_stats("PAC12",PRO_SPEED_CATEGORY_NUM,conference_dict),columns=["PAC12"])
+    BIG12speed_df = pd.DataFrame(get_conf_stats("BIG12",PRO_SPEED_CATEGORY_NUM,conference_dict),columns=["BIG12"])
+    plot_speedgraph(SECspeed_df,BIG10speed_df,ACCspeed_df,PAC12speed_df,BIG12speed_df)
+
+
 
 ### ----------------------------------------------------------------------------------------------------
 ### PAIRWISE_PLOT
@@ -47,6 +67,30 @@ def height_conversion(height_entry):
 ### GENERAL FUNCTIONS
 ### ----------------------------------------------------------------------------------------------------
 
+def sort_conf_players(conn,conference):
+    dict = {}
+    cur = conn.cursor()
+    for college in conference:
+        cur.execute("SELECT * FROM URL WHERE COLLEGE = ' " + college + "'")
+        dict[college] = cur.fetchall()
+    return dict
+
+def sort_by_conference(conn):
+    SEC = ["Alabama","Florida","Georgia","Missouri","South Carolina","Tennessee","Vanderbilt","Alabama","Arkansas","Auburn","LSU","Mississippi","Mississippi State","Texas AM"]
+    BIG10 = ["Indiana","Maryland","Michigan","Michigan State","Ohio State","Penn State","Rutgers","Illinois","Iowa","Minnesota","Nebraska","Northwestern","Purdue","Wisconsin"]
+    ACC = ["Boston College","Clemson","Florida State","Louisville","North Carolina State","Syracuse","Wake Forest","Duke","Georgia Tech","Miami","North Carolina","Pittsburgh","Virginia","Virginia Tech"]
+    PAC12 = ["Arizona","Arizona State","California","UCLA","Colorado","Oregon","Oregon State","Southern California","Utah","Washington","Washington State"]
+    BIG12 = ["Baylor","Iowa State","Kansas","Kansas State","Oklahoma","Oklahoma State","Texas Christian","Texas","Texas Tech","West Virginia"]
+    
+    conference_dict = {}
+    conference_dict["SEC"] = sort_conf_players(conn,SEC)
+    conference_dict["BIG10"] = sort_conf_players(conn,BIG10)
+    conference_dict["ACC"] = sort_conf_players(conn,ACC)
+    conference_dict["PAC12"] = sort_conf_players(conn,PAC12)
+    conference_dict["BIG12"] = sort_conf_players(conn,BIG12)
+    
+    return conference_dict
+
 def get_column(conn,col_name):
     cur = conn.cursor()
     cur.execute("SELECT " + col_name+ " FROM URL")
@@ -66,12 +110,10 @@ def create_connection(db):
     except Error as e:
         print(e)
 
-# URL TEXT,NAME TEXT,POSITION TEXT,COLLEGE TEXT,CB_INVITE TEXT,CB_HEIGHT TEXT,CB_WEIGHT TEXT,CB_FORTY TEXT,CB_TWENTY TEXT,CB_TEN TEXT,CB_REPS TEXT,CB_VERT TEXT,CB_BROAD TEXT,CB_SHUTTLE TEXT,CB_DRILL TEXT, PRO_INVITE TEXT, PRO_HEIGHT TEXT,PRO_WEIGHT TEXT,PRO_FORTY TEXT,PRO_TWENTY TEXT,PRO_TEN TEXT,PRO_REPS TEXT,PRO_VERT TEXT,PRO_BROAD TEXT,PRO_SHUTTLE TEXT,PRO_DRILL TEXT
-
 def main():
     db = "/Users/davidta/Desktop/nfl_prospect/nds_data/nfl_players.db"
     conn = create_connection(db)
-    pairwise_plot(conn)
+    #pairwise_plot(conn)
     speed_comp(conn)
 
 if __name__ == '__main__':
--- sr_data/sr_scrape.py
+++ sr_data/sr_scrape.py
@@ -5,11 +5,17 @@ import re
 
 def get_data():
     for letter in range(ord('a'),ord('b')):
-        #page = requests.get("https://www.sports-reference.com/cfb/players/" + str(letter) + "-index.html")
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-        page = requests.get("https://www.sports-reference.com/cfb/players/a-index.html",headers=headers)
-        #soup = BeautifulSoup(page.content, 'html.parser')
-        #print soup.body.div.div.span
+        page = requests.get("https://www.sports-reference.com/cfb/players/" + chr(letter)+"-index.html")
+        soup = BeautifulSoup(page.content,"html.parser")
+        p_soup = soup.findAll('p')
+        for i in p_soup:
+            print i
+        page_index = 2
+        while(True):
+            page = requests.get("https://www.sports-reference.com/cfb/players/" + chr(letter) + "-index-" + str(page_index) + ".html")
+            if str(page) == "<Response [404]>":
+                    return
+            page_index = page_index + 1
     return 0;
 
 def main():
