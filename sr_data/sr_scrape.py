8f3f1b45100fef6e61effaf0fd1f6bc0e5b81153
David Ta <dta255@stanford.edu>
2017-12-22 12:55:55 -0800

    get scoring

--- sr_data/sr_scrape.py
+++ sr_data/sr_scrape.py
@@ -99,16 +99,106 @@ def parse_passing(tr,pass_stats):
     pass_stats[year] = pass_stats_entry
     return pass_stats;
 
+def rushing_receiving(tr,rr_stats):
+    year = int(parse_year(tr))
+    rr_stats_entry = []
+    
+    ## School
+    rr_stats_entry = stats_search('\"\/cfb\/schools\/\w+\/\d+\.html\"\>\w+\<\/a',rr_stats_entry,tr)
+    ## Conferences
+    rr_stats_entry = stats_search('\"\/cfb\/conferences\/.*?\/\d+\.html\"\>.*?\<\/a',rr_stats_entry,tr)
+    ## Class
+    rr_stats_entry = stats_search('class\"\s\>\w+\<\/',rr_stats_entry,tr)
+    ## Position Played
+    rr_stats_entry = stats_search('\"pos\"\s\>\w+\<\/td',rr_stats_entry,tr)
+    ## Games
+    rr_stats_entry = stats_search('stat\=\"g\"\s\>\d+\<\/td',rr_stats_entry,tr)
+    ## Rush Attempts
+    rr_stats_entry = stats_search('\"rush\_att\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Rushing Yards
+    rr_stats_entry = stats_search('\"rush\_yds\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Rushing Yards Per Attempt
+    rr_stats_entry = stats_search('\"rush\_yds\_per\_att\"\s\>\d+\.\d+\<\/td\>',rr_stats_entry,tr)
+    ## Rushing Touchdowns
+    rr_stats_entry = stats_search('\"rush\_td\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Receptions
+    rr_stats_entry = stats_search('\"rec\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Receiving Yards
+    rr_stats_entry = stats_search('\"rec\_yds\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Receiving Yards Per Reception
+    rr_stats_entry = stats_search('\"rec\_yds\_per\_rec\"\s\>\d+\.\d+\<\/td\>',rr_stats_entry,tr)
+    ## Receiving Touchdowns
+    rr_stats_entry = stats_search('\"rec\_td\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Plays From Scrimmage
+    rr_stats_entry = stats_search('\"scrim\_att\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Yards From Scrimmage
+    rr_stats_entry = stats_search('\"scrim\_yds\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    ## Yards From Scrimmage Per Play
+    rr_stats_entry = stats_search('\"scrim\_yds\_per\_att\"\s\>\d+\.\d+\<\/td\>',rr_stats_entry,tr)
+    ## Touchdowns From Scrimmage
+    rr_stats_entry = stats_search('\"scrim\_td\"\s\>\d+\<\/td\>',rr_stats_entry,tr)
+    
+    rr_stats[year] = rr_stats_entry
+    return rr_stats
+
+def scoring(tr,scoring_stats):
+    year = int(parse_year(tr))
+    scoring_stats_entry = []
+    
+    ## School
+    scoring_stats_entry = stats_search('\"\/cfb\/schools\/\w+\/\d+\.html\"\>\w+\<\/a',scoring_stats_entry,tr)
+    ## Conferences
+    scoring_stats_entry = stats_search('\"\/cfb\/conferences\/.*?\/\d+\.html\"\>.*?\<\/a',scoring_stats_entry,tr)
+    ## Class
+    scoring_stats_entry = stats_search('class\"\s\>\w+\<\/',scoring_stats_entry,tr)
+    ## Position Played
+    scoring_stats_entry = stats_search('\"pos\"\s\>\w+\<\/td',scoring_stats_entry,tr)
+    ## Games
+    scoring_stats_entry = stats_search('stat\=\"g\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Rushing Touchdowns
+    scoring_stats_entry = stats_search('\"td\_rush\"\s\>\d+\<\/td\>',scoring_stats_entry,tr)
+    ## Receiving Touchdowns
+    scoring_stats_entry = stats_search('\"td\_rec\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Interception Return Touchdowns
+    scoring_stats_entry = stats_search('\"td\_def\_int\"\s\>\d+\<\/td\>',scoring_stats_entry,tr)
+    ## Fumble Recovery Return Touchdowns
+    scoring_stats_entry = stats_search('\"td\_fumbles\_rec\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Punt Return Touchdowns
+    scoring_stats_entry = stats_search('\"td\_punt\_ret\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Kick Return Touchdowns
+    scoring_stats_entry = stats_search('\"td\_kick\_ret\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Other Touchdowns
+    scoring_stats_entry = stats_search('\"td\_other\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Total Touchdowns
+    scoring_stats_entry = stats_search('=\td\_total\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Punt Return Touchdowns
+    scoring_stats_entry = stats_search('\"xpm\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Kick Return Touchdowns
+    scoring_stats_entry = stats_search('\"fgm\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Other Touchdowns
+    scoring_stats_entry = stats_search('\"two\_pt\_md\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Total Touchdowns
+    scoring_stats_entry = stats_search('\"safety\_md\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    ## Total Touchdowns
+    scoring_stats_entry = stats_search('\"points\"\s\>\d+\<\/td',scoring_stats_entry,tr)
+    
+    scoring_stats[year] = scoring_stats_entry
+    return scoring_stats
+
 def player_stats(player_url,years_active,player_name,categories):
     page = requests.get("https://www.sports-reference.com" + player_url)
-    pass_stats = def_stats = {}
+    scoring_stats=rr_stats=pass_stats=def_stats={}
     trs = re.findall('\<tr\s\>.*?\<\/tr\>',page.content)
     for tr in trs:
         if re.search('tackles_assists',tr) != None:
             def_stats = parse_defense(tr,def_stats)
         elif re.search('pass_att',tr) != None:
             pass_stats = parse_passing(tr,pass_stats)
-            print pass_stats
+        elif re.search('rush_att',tr) != None:
+            rr_stats = rushing_receiving(tr,rr_stats)
+        elif re.search('td_def_int',tr) != None:
+            scoring_stats = scoring(tr,scoring_stats)
+            print scoring_stats
 
 
 ### school = re.sub("[^a-zA-Z]+", "", raw_school.group(0))
