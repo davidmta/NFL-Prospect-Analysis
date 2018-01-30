fd3f872933a1b2b5d21527c41a3dfebad2cd7dd2
David Ta <dta255@stanford.edu>
2018-01-30 10:57:59 -0800

    set up requests

--- nfl_sr_data/nfl_sr_scrape.py
+++ nfl_sr_data/nfl_sr_scrape.py
@@ -1,14 +1,25 @@
+import requests
+import sys
+import sqlite3 as lite
 
+def get_players_stats(page,cur):
+	soup = BeautifulSoup(page.content,"html.parser")
+	p_soup = soup.findAll('p')
+	
+def get_data(cur):
+	for letter in range(ord('A'),ord('Z')+1):
+		page = requests.get("https://www.pro-football-reference.com/players/" + chr(letter))
+		get_players_stats(page,cur)
 
-def attempt_connection(start,end):
-    try:
-        con = lite.connect('sr_players_database_' + start + '_' + end + '.db')
-        return con
-    except Error as e:
-        print(e)
+def attempt_connection():
+    con = lite.connect('sr_nfl_players_database.db')
+    return con
 
 def main():
-	con = attempt_connection(start,end)
+	con = attempt_connection()
+	with con:
+		cur = con.cursor()
+		get_data(cur)
 
 if __name__ == "__main__":
     main()
